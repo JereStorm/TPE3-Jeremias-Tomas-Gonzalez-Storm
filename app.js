1b3294f3377d2e5d1e6ab220638cd5bd5cfea53c
Jeremias Storm <jerestorm@gmail.com>
2023-04-25 16:29:54 -0300

    se agregan filtros grey, invert, sepia

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const rect = canvas.getBoundingClientRect();
 const width = canvas.width;
 const height = canvas.height;
 
+const reset_btn = document.getElementById("reset");
 const pencil = document.getElementById("pencil");
 const eraser = document.getElementById("eraser");
 const color_sl = document.getElementById("color-selector");
 const range_sl = document.getElementById("range");
+const file_sl = document.getElementById("file_sl");
+const img = new Image();
 
 let posX = 0;
 let posY = 0;
@@ -19,6 +22,20 @@ let drawing = false;
 
 let color = color_sl.value;
 let range = range_sl.value;
+
+//------------------------------- MANEJANDO EVENTOS ---------------------------------------//
+
+reset_btn.addEventListener('click', function () {
+    file_sl.value = null;
+    cleanCanvas();
+})
+
+file_sl.addEventListener('change', () => {
+    img.src = URL.createObjectURL(file_sl.files[0]);
+    img.onload = () => {
+        myDrawImage();
+    }
+})
 
 pencil.addEventListener('click', () => {
     if (eraser.classList.contains('selected')) {
@@ -67,6 +84,9 @@ canvas.addEventListener("mouseup", function (e) {
         drawing = false;
     }
 })
+
+//---------------------------- FUNCIONES DEL DRAW ------------------------------------//
+
 function main() {
     //do something
     cleanCanvas();
@@ -90,6 +110,9 @@ function draw(finX, finY) {
     ctx.closePath();
 
 }
+function myDrawImage() {
+    ctx.drawImage(img, width / 2 - img.width / 2, height / 2 - img.height / 2);
+}
 
 function setMousePos(evt) {
     posX = evt.clientX - rect.left;
@@ -98,7 +121,6 @@ function setMousePos(evt) {
 
 function setColor(c) {
     if (!eraser.classList.contains('selected')) {
-
         color = c;
     } else {
         color = 'white'
@@ -107,4 +129,60 @@ function setColor(c) {
 
 function setRange(g) {
     range = g;
+}
+
+//------------------------------------ FILTROS ------------------------------------------//
+
+function addFilterGrey() {
+    if (img.src == '') {
+        return
+    }
+
+    let imageData = ctx.getImageData(width / 2 - img.width / 2, height / 2 - img.height / 2, img.width, img.height);
+
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        const gris = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        data[i] = gris; // red
+        data[i + 1] = gris; // green
+        data[i + 2] = gris; // blue
+    }
+
+    ctx.putImageData(imageData, width / 2 - img.width / 2, height / 2 - img.height / 2);
+}
+function addFilterInvert() {
+    if (img.src == '') {
+        return
+    }
+
+    let imageData = ctx.getImageData(width / 2 - img.width / 2, height / 2 - img.height / 2, img.width, img.height);
+
+    const data = imageData.data;
+
+
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i]; // red
+        data[i + 1] = 255 - data[i + 1]; // green
+        data[i + 2] = 255 - data[i + 2]; // blue
+    }
+
+    ctx.putImageData(imageData, width / 2 - img.width / 2, height / 2 - img.height / 2);
+}
+function addFilterSepia() {
+    if (img.src == '') {
+        return
+    }
+
+    let imageData = ctx.getImageData(width / 2 - img.width / 2, height / 2 - img.height / 2, img.width, img.height);
+
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = Math.min((data[i] * 0.393) + (data[i + 1] * 0.769) + (data[i + 2] * 0.189), 255); // red
+        data[i + 1] = Math.min((data[i] * 0.349) + (data[i + 1] * 0.686) + (data[i + 2] * 0.168), 255); // green
+        data[i + 2] = Math.min((data[i] * 0.272) + (data[i + 1] * 0.534) + (data[i + 2] * 0.131), 255); // blue
+    }
+
+    ctx.putImageData(imageData, width / 2 - img.width / 2, height / 2 - img.height / 2);
 }
